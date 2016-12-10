59414a5e28da44fe2ea39b63047b8261f9a5d8f8
TheoKoenig <theocuseceo@gmail.com>
2016-12-10 09:40:37 +0100

    Add files via upload

--- 404.php
+++ 404.php
@@ -5,10 +5,10 @@
             <img
                 src="https://mmi-projet.pu-pm.univ-fcomte.fr/projets-co1617/projet1617_10/wp/wp-content/uploads/2016/11/logo_eco.png"
                 alt="Logo Ecocampus">
-            <p>Nous sommes désolé, la page que vous avez demandée n’a pas été trouvée.</p>
-            <p>Il se peut que le lien que vous avez utilisé soit rompu ou que vous ayez tapé l’adresse (URL)
-                incorrectement.</p>
-            <a class='bouton' href="<?php echo esc_url(home_url()); ?>">Retourner sur à l'accueil</a>
+            <p>Ce qu'il se passe sur cette page c'est pas vos oignons...<br>
+                Alors on va pas se prendre le chou et vous allez recharger la page !<br>
+                Allez appuyez sur le champignon quoi !</p>
+            <a class='bouton' href="<?php echo esc_url(home_url()); ?>">Retourner à l'accueil</a>
     </div>
 </main>
 <?php get_footer(); ?>
