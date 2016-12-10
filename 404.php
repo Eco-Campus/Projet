d55d9d11dbc5bbe663ad174ffda857b18487a7a7
TheoKoenig <theocuseceo@gmail.com>
2016-12-10 09:27:53 +0100

    Fichier 404 mis à jour

--- 404.php
+++ 404.php
@@ -1,15 +1,14 @@
 <?php get_header(); ?>
 
 <main>
-    <div class="taille">
-        <div id="404">
+    <div class="erreur">
             <img
                 src="https://mmi-projet.pu-pm.univ-fcomte.fr/projets-co1617/projet1617_10/wp/wp-content/uploads/2016/11/logo_eco.png"
                 alt="Logo Ecocampus">
             <p>Nous sommes désolé, la page que vous avez demandée n’a pas été trouvée.</p>
             <p>Il se peut que le lien que vous avez utilisé soit rompu ou que vous ayez tapé l’adresse (URL)
                 incorrectement.</p>
-        </div>
+            <a class='bouton' href="<?php echo esc_url(home_url()); ?>">Retourner sur à l'accueil</a>
     </div>
 </main>
 <?php get_footer(); ?>
